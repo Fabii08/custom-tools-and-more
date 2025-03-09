f9397b5aad74e77f839307c273b7bf6004c899cd
Fabii <fabianschhdh@gmail.com>
2025-03-09 18:38:41 +0100

    Update ThreeByThreeShovelListener.java

--- src/main/java/de/fabi/customToolsAndMore/Listener/ThreeByThreeShovelListener.java
+++ src/main/java/de/fabi/customToolsAndMore/Listener/ThreeByThreeShovelListener.java
@@ -1,13 +1,9 @@
-package de.fabi.customToolsAndMore.Listener;
 /*
-Copyright © 2024 https://github.com/Fabii08?tab=repositories
-All rights reserved.
+Copyright © 2025 https://github.com/Fabii08?tab=repositories  
+All rights reserved.  
+*/ 
+package de.fabi.customToolsAndMore.Listener;
 
-Unauthorized copying, modification, or distribution of this file,
-via any medium, is strictly prohibited.
-
-DO NOT DISTRIBUTE.
-*/
 
 import org.bukkit.Location;
 import org.bukkit.Material;
